90db367d6463c97716ed067d21a67c4a9913df06
Cori <corinne.a.ader@gmail.com>
2019-08-10 23:49:41 -0400

    final udpates before submission

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -4,6 +4,8 @@
 window.onload = function() {
 	let objs;
 	let dropDownList = document.getElementById('mtnSelect');
+
+	//retrieving data from json file
 	$.getJSON('data/mountains.json', function(data) {
 		objs = data;
 		for (let i = 0; i < objs.mountains.length; i++) {
@@ -13,10 +15,13 @@ window.onload = function() {
 			dropDownList.appendChild(dropDownoptions);
 		}
 	});
+
 	//dropdown list on change event handler (when user makes a selection)
 	dropDownList.onchange = function() {
 		//mountain variables pulled from json object
-		let selection = objs.mountains[mtnSelect.selectedIndex];
+		let mtnJtron = document.getElementById('mtnJtron');
+		mtnJtron.style.display = 'none';
+		let selection = objs.mountains[mtnSelect.selectedIndex - 1];
 		let mtnName = selection.name;
 		let mtnElev = selection.elevation;
 		let mtnEffort = selection.effort;
@@ -31,14 +36,17 @@ window.onload = function() {
 
 		//sunrise & sunset time API by long/lat
 		$.getJSON(concatURL, function(data) {
-			//time variables pulled from API 
+			//time variables pulled from API
 			timeObj = data;
 			sunriseTime = timeObj.results.sunrise;
 			sunsetTime = timeObj.results.sunset;
 
 			//time Conversion API url
 			let codeLookupURL =
-				'http://api.timezonedb.com/v2.1/get-time-zone?key=Z5NMTI9BSFUC&format=json&by=position&lat=' + mtnLat + '&lng=' + mtnLong;
+				'http://api.timezonedb.com/v2.1/get-time-zone?key=Z5NMTI9BSFUC&format=json&by=position&lat=' +
+				mtnLat +
+				'&lng=' +
+				mtnLong;
 			let timeConversionObj;
 
 			//API for looking up time zone difference from coordinates
@@ -47,23 +55,28 @@ window.onload = function() {
 				let timeOffset = timeConversionObj.gmtOffset;
 				let timeOffsetMS = timeOffset * 1000;
 				//splitting off hours to adjust for timezone
-				let riseTime = new Date("01-01-1970 " + sunriseTime);
+				let riseTime = new Date('01-01-1970 ' + sunriseTime);
 				let riseTimeMS = Date.parse(riseTime);
 				let localRiseTimeMS = riseTimeMS + timeOffsetMS;
 				let localRiseTime = new Date(localRiseTimeMS);
 				let localRiseHour = localRiseTime.getHours();
 				let localRiseMin = localRiseTime.getMinutes();
 				let localRiseSec = localRiseTime.getSeconds();
-				let localRiseOutputTime = (localRiseHour + ":" + localRiseMin + ":" + localRiseSec + " AM");
-
-				let setTime = new Date("01-01-1970 " + sunsetTime);
+				if (localRiseSec < 10) {
+					localRiseSec = new String('0' + localRiseSec);
+				}
+				let localRiseOutputTime = localRiseHour + ':' + localRiseMin + ':' + localRiseSec + ' AM';
+				let setTime = new Date('01-01-1970 ' + sunsetTime);
 				let setTimeMS = Date.parse(setTime);
 				let localSetTimeMS = setTimeMS + timeOffsetMS;
 				let localSetTime = new Date(localSetTimeMS);
 				let localSetHour = localSetTime.getHours();
 				let localSetMin = localSetTime.getMinutes();
 				let localSetSec = localSetTime.getSeconds();
-				let localSetOutputTime = ((localSetHour-12) + ":" + localSetMin + ":" + localSetSec + " PM");
+				if (localSetSec < 10) {
+					localSetSec = new String('0' + localSetSec);
+				}
+				let localSetOutputTime = localSetHour - 12 + ':' + localSetMin + ':' + localSetSec + ' PM';
 
 				//row7 - Today's Sunrise
 				let row7 = displayTable.insertRow(displayTable.rows.length);
@@ -80,7 +93,7 @@ window.onload = function() {
 				cell2_8.innerHTML = localSetOutputTime;
 			});
 		});
-
+		//clears table when new selection is made
 		let displayTable = document.getElementById('mtnTblOutput');
 		while (displayTable.childNodes.length) {
 			displayTable.innerHTML = '';
@@ -88,9 +101,10 @@ window.onload = function() {
 		//row1  - Name
 		let row1 = displayTable.insertRow(displayTable.rows.length);
 		let cell1_1 = row1.insertCell(0);
-		cell1_1.innerHTML = 'Name';
-		let cell2_1 = row1.insertCell(1);
-		cell2_1.innerHTML = mtnName;
+		cell1_1.innerHTML = mtnName;
+		cell1_1.className = 'bg-info text-center text-white mtnName';
+		cell1_1.style.fontSize = 'x-large';
+		cell1_1.colSpan = '2';
 
 		//row2  - Elevation
 		let row2 = displayTable.insertRow(displayTable.rows.length);
@@ -112,6 +126,7 @@ window.onload = function() {
 		cell1_4.innerHTML = 'Photo';
 		let imgSrc = document.createElement('img');
 		imgSrc.src = 'images/' + mtnImg;
+		imgSrc.alt = 'mountain photo';
 		let cell2_4 = row4.insertCell(1);
 		cell2_4.appendChild(imgSrc);
 
